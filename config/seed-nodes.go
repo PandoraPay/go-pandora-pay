2d9ec84790f9047a6ab0affbcdfec157c339dd7f
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-06-01 02:06:38 +0300

    testnet seed nodes

--- config/seed-nodes.go
+++ config/seed-nodes.go
@@ -3,9 +3,18 @@ package config
 import "net/url"
 
 var (
-	MAIN_NET_SEED_NODES = []url.URL{{Scheme: "ws", Host: "127.0.0.1:5231", Path: "/ws"}}
+	MAIN_NET_SEED_NODES = []url.URL{
+		{Scheme: "ws", Host: "127.0.0.1:5231", Path: "/ws"},
+	}
 
-	TEST_NET_SEED_NODES = []url.URL{{Scheme: "ws", Host: "127.0.0.1:5231", Path: "/ws"}}
+	TEST_NET_SEED_NODES = []url.URL{
+		{Scheme: "ws", Host: "helloworldx.ddns.net:6000", Path: "/ws"},
+		{Scheme: "ws", Host: "helloworldx.ddns.net:6001", Path: "/ws"},
+		{Scheme: "ws", Host: "helloworldx.ddns.net:6002", Path: "/ws"},
+		{Scheme: "ws", Host: "helloworldx.ddns.net:6003", Path: "/ws"},
+	}
 
-	DEV_NET_SEED_NODES = []url.URL{{Scheme: "ws", Host: "127.0.0.1:5231", Path: "/ws"}}
+	DEV_NET_SEED_NODES = []url.URL{
+		{Scheme: "ws", Host: "127.0.0.1:5231", Path: "/ws"},
+	}
 )
