5a729fc20264a296e05d0147098c9e1b812d90e5
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-06-02 02:09:02 +0300

    script compiling and gzipping #2

--- scripts/compile-wasm-wallet.sh
+++ scripts/compile-wasm-wallet.sh
@@ -3,6 +3,12 @@ echo "Deleting..."
 rm ./webassembly/dist/PandoraPay-wallet.wasm.gz
 echo "Compiling..."
 GOOS=js GOARCH=wasm go build -o ./webassembly/dist/PandoraPay-wallet.wasm
+
+echo "Copy to wallet/dev..."
+cp ./webassembly/dist/PandoraPay-wallet.wasm ../PandoraPay-wallet/dist/dev
+echo "Copy to wallet/build..."
+cp ./webassembly/dist/PandoraPay-wallet.wasm ../PandoraPay-wallet/dist/build
+
 echo "Gzipping..."
 gzip --best ./webassembly/dist/PandoraPay-wallet.wasm
 echo "Copy to wallet/dev..."
