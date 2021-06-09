d3512e93964b962af6c44ac5ea14d8b26003cd23
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-06-09 17:33:27 +0300

    better scripting shell

--- scripts/compile-wasm-wallet-dev.sh
+++
@@ -1,2 +0,0 @@
-GOOS=js GOARCH=wasm go build -o ./webassembly/dist/PandoraPay-wallet.wasm
-cp ./webassembly/dist/PandoraPay-wallet.wasm ../PandoraPay-wallet/dist/dev
--- scripts/compile-wasm.sh
+++ scripts/compile-wasm.sh
@@ -1,1 +1,50 @@
-GOOS=js GOARCH=wasm go build -o ../webassembly/dist/PandoraPay.wasm
+if [ $# -eq 0 ]
+then
+  echo "argument missing"
+  exit 1
+fi
+
+
+if [ $1 == "dev" ]
+then
+  GOOS=js GOARCH=wasm go build -o ../webassembly/dist/PandoraPay.wasm
+  exit 1
+fi
+
+if [ $1 == "wallet-dev" ]
+then
+  GOOS=js GOARCH=wasm go build -o ./webassembly/dist/PandoraPay-wallet.wasm
+  cp ./webassembly/dist/PandoraPay-wallet.wasm ../PandoraPay-wallet/dist/dev
+  exit 1
+fi
+
+if [ $1 == "wallet-build" ]
+then
+  echo "Deleting..."
+  rm ./webassembly/dist/PandoraPay-wallet.wasm.gz
+  rm ./webassembly/dist/PandoraPay-wallet.wasm.br
+
+  echo "Compiling..."
+  GOOS=js GOARCH=wasm go build -o ./webassembly/dist/PandoraPay-wallet.wasm
+
+  echo "Copy to wallet/build..."
+  cp ./webassembly/dist/PandoraPay-wallet.wasm ../PandoraPay-wallet/dist/build
+
+  if [ $2 == "brotli" ]
+  then
+    echo "Gzipping using brotli..."
+    brotli -o ./webassembly/dist/PandoraPay-wallet.wasm.br ./webassembly/dist/PandoraPay-wallet.wasm
+    echo "Copy to wallet/build..."
+    cp ./webassembly/dist/PandoraPay-wallet.wasm.gz ../PandoraPay-wallet/dist/build
+  else
+    echo "Gzipping..."
+    gzip --best ./webassembly/dist/PandoraPay-wallet.wasm
+    echo "Copy to wallet/build..."
+    cp ./webassembly/dist/PandoraPay-wallet.wasm.gz ../PandoraPay-wallet/dist/build
+  fi
+
+  exit 1
+fi
+
+
+echo "'dev', 'wallet-dev', 'wallet-build | brotli'"
