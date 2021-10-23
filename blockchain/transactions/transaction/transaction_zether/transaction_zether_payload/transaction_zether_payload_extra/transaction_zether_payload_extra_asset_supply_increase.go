6feb1d51837e0931204aab202e2528c842fccc23
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-10-24 01:37:31 +0300

    Supply increase implementation

--- blockchain/transactions/transaction/transaction_zether/transaction_zether_payload/transaction_zether_payload_extra/transaction_zether_payload_extra_asset_supply_increase.go
+++ blockchain/transactions/transaction/transaction_zether/transaction_zether_payload/transaction_zether_payload_extra/transaction_zether_payload_extra_asset_supply_increase.go
@@ -13,10 +13,11 @@ import (
 
 type TransactionZetherPayloadExtraAssetSupplyIncrease struct {
 	TransactionZetherPayloadExtraInterface
-	AssetId           []byte
-	ReceiverPublicKey []byte //must be registered before
-	Value             uint64
-	AssetSignature    []byte
+	AssetId              []byte
+	ReceiverPublicKey    []byte //must be registered before
+	Value                uint64
+	AssetSupplyPublicKey []byte
+	AssetSignature       []byte
 }
 
 func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) BeforeIncludeTxPayload(txRegistrations *transaction_zether_registrations.TransactionZetherDataRegistrations, payloadIndex byte, payloadAsset []byte, payloadBurnValue uint64, payloadStatement *crypto.Statement, publicKeyList [][]byte, blockHeight uint64, dataStorage *data_storage.DataStorage) (err error) {
@@ -24,7 +25,66 @@ func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) BeforeIncl
 }
 
 func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) IncludeTxPayload(txRegistrations *transaction_zether_registrations.TransactionZetherDataRegistrations, payloadIndex byte, payloadAsset []byte, payloadBurnValue uint64, payloadStatement *crypto.Statement, publicKeyList [][]byte, blockHeight uint64, dataStorage *data_storage.DataStorage) (err error) {
+
+	ast, err := dataStorage.Asts.GetAsset(payloadExtra.AssetId)
+	if err != nil {
+		return
+	}
+
+	if ast == nil {
+		return errors.New("Asset was not found")
+	}
+
+	if !bytes.Equal(payloadExtra.AssetSupplyPublicKey, ast.SupplyPublicKey) {
+		return errors.New("Asset SupplyPublicKey is not matching")
+	}
+
+	accs, err := dataStorage.AccsCollection.GetMap(payloadExtra.AssetId)
+	if err != nil {
+		return
+	}
+	if accs == nil {
+		return errors.New("Accs was not found")
+	}
+
+	isReg, err := dataStorage.Regs.Exists(string(payloadExtra.ReceiverPublicKey))
+	if err != nil {
+		return
+	}
+	if !isReg {
+		return errors.New("Receiver Public Key is not registered")
+	}
+
+	acc, err := accs.GetAccount(payloadExtra.ReceiverPublicKey)
+	if err != nil {
+		return
+	}
+
+	if acc == nil {
+		if acc, err = accs.CreateAccount(payloadExtra.ReceiverPublicKey); err != nil {
+			return
+		}
+	}
+
+	if err = ast.AddSupply(true, payloadExtra.Value); err != nil {
+		return
+	}
+	if err = acc.Balance.AddBalanceUint(payloadExtra.Value); err != nil {
+		return
+	}
+
+	if err = accs.Update(string(payloadExtra.ReceiverPublicKey), acc); err != nil {
+		return
+	}
+	if err = dataStorage.Asts.UpdateAsset(payloadExtra.AssetId, ast); err != nil {
+		return
+	}
+
 	return
+}
+
+func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) VerifyExtraSignature(hashForSignature []byte) bool {
+	return crypto.VerifySignature(hashForSignature, payloadExtra.AssetSignature, payloadExtra.AssetSupplyPublicKey)
 }
 
 func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) Validate(txRegistrations *transaction_zether_registrations.TransactionZetherDataRegistrations, payloadIndex byte, payloadAsset []byte, payloadBurnValue uint64, payloadStatement *crypto.Statement) error {
@@ -34,6 +94,12 @@ func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) Validate(t
 	if !bytes.Equal(payloadAsset, config_coins.NATIVE_ASSET_FULL) {
 		return errors.New("payloadAsset must be NATIVE_ASSET_FULL")
 	}
+	if len(payloadExtra.ReceiverPublicKey) != cryptography.PublicKeySize || len(payloadExtra.AssetSupplyPublicKey) != cryptography.PublicKeySize {
+		return errors.New("Invalid Public Keys")
+	}
+	if len(payloadExtra.AssetSignature) != cryptography.SignatureSize {
+		return errors.New("Invalid Signature")
+	}
 	return nil
 }
 
@@ -41,6 +107,7 @@ func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) Serialize(
 	w.Write(payloadExtra.AssetId)
 	w.Write(payloadExtra.ReceiverPublicKey)
 	w.WriteUvarint(payloadExtra.Value)
+	w.Write(payloadExtra.AssetSupplyPublicKey)
 	if inclSignature {
 		w.Write(payloadExtra.AssetSignature)
 	}
@@ -54,6 +121,9 @@ func (payloadExtra *TransactionZetherPayloadExtraAssetSupplyIncrease) Deserializ
 		return
 	}
 	if payloadExtra.Value, err = r.ReadUvarint(); err != nil {
+		return
+	}
+	if payloadExtra.AssetSupplyPublicKey, err = r.ReadBytes(cryptography.PublicKeySize); err != nil {
 		return
 	}
 	if payloadExtra.AssetSignature, err = r.ReadBytes(cryptography.SignatureSize); err != nil {
