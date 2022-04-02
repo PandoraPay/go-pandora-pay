edb3c3deb3f0aac66172e522b914d2ce6fc83468
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2022-04-02 17:29:58 +0300

    addresses unit testing improved and fixed.

--- addresses/private_key_test.go
+++ addresses/private_key_test.go
@@ -14,6 +14,7 @@ func TestGenerateNewPrivateKey(t *testing.T) {
 	privateKey := GenerateNewPrivateKey()
 	assert.Equal(t, len(privateKey.Key), 32, "Invalid private key length")
 	assert.NotEqual(t, privateKey.Key, helpers.EmptyBytes(32), "Invalid private key is empty")
+	assert.Equal(t, bytes.Equal(privateKey.Key, privateKey.GeneratePublicKey()), false)
 
 }
 
@@ -21,35 +22,38 @@ func TestPrivateKey_GenerateAddress(t *testing.T) {
 
 	privateKey := GenerateNewPrivateKey()
 
-	address, err := privateKey.GenerateAddress(0, helpers.EmptyBytes(0))
+	address, err := privateKey.GenerateAddress(false, nil, false, nil, 0, nil)
 	assert.NoError(t, err)
 
-	assert.Equal(t, len(address.PublicKey), cryptography.PublicKeySize, "Generated Address is invalid")
-	assert.NotEqual(t, address.PublicKey, helpers.EmptyBytes(cryptography.PublicKeySize), "Generated Address is invalid")
-	assert.Equal(t, address.Amount, uint64(0), "Generated Address is invalid")
-	assert.Equal(t, len(address.PaymentID), 0, "Generated Address is invalid")
+	assert.Equal(t, bytes.Equal(address.PublicKey, privateKey.GeneratePublicKey()), true)
+	assert.Equal(t, bytes.Equal(address.PublicKey, privateKey.Key), false)
 
-	address, err = privateKey.GenerateAddress(0, helpers.EmptyBytes(0))
+	assert.Equal(t, len(address.PublicKey), cryptography.PublicKeySize)
+	assert.Equal(t, false, bytes.Equal(address.PublicKey, helpers.EmptyBytes(cryptography.PublicKeySize)))
+	assert.Equal(t, address.PaymentAmount, uint64(0))
+	assert.Equal(t, len(address.PaymentID), 0)
+
+	address, err = privateKey.GenerateAddress(false, nil, false, nil, 0, nil)
 	assert.NoError(t, err)
 
-	assert.Equal(t, len(address.PublicKey), cryptography.PublicKeySize, "Generated Address is invalid")
-	assert.NotEqual(t, address.PublicKey, helpers.EmptyBytes(cryptography.PublicKeySize), "Generated Address is invalid")
-	assert.Equal(t, address.Amount, uint64(0), "Generated Address is invalid")
-	assert.Equal(t, len(address.PaymentID), 0, "Generated Address is invalid")
+	assert.Equal(t, len(address.PublicKey), cryptography.PublicKeySize)
+	assert.Equal(t, bytes.Equal(address.PublicKey, helpers.EmptyBytes(cryptography.PublicKeySize)), false)
+	assert.Equal(t, address.PaymentAmount, uint64(0))
+	assert.Equal(t, len(address.PaymentID), 0)
 
-	address, err = privateKey.GenerateAddress(20, helpers.RandomBytes(8))
+	address, err = privateKey.GenerateAddress(false, nil, false, helpers.RandomBytes(8), 20, nil)
 	assert.NoError(t, err)
 
-	assert.Equal(t, len(address.PublicKey), cryptography.PublicKeySize, "Generated Address is invalid")
-	assert.NotEqual(t, address.PublicKey, helpers.EmptyBytes(cryptography.PublicKeySize), "Generated Address is invalid")
-	assert.Equal(t, address.Amount, uint64(20), "Generated Address is invalid")
-	assert.Equal(t, len(address.PaymentID), 8, "Generated Address is invalid")
+	assert.Equal(t, len(address.PublicKey), cryptography.PublicKeySize)
+	assert.NotEqual(t, address.PublicKey, helpers.EmptyBytes(cryptography.PublicKeySize))
+	assert.Equal(t, address.PaymentAmount, uint64(20))
+	assert.Equal(t, len(address.PaymentID), 8)
 
 }
 
 func TestPrivateKey_BN256(t *testing.T) {
 	privateKey := GenerateNewPrivateKey()
-	address, err := privateKey.GenerateAddress(0, helpers.EmptyBytes(0))
+	address, err := privateKey.GenerateAddress(false, nil, false, nil, 0, helpers.EmptyBytes(0))
 	assert.NoError(t, err)
 
 	priv := new(crypto.BNRed).SetBytes(privateKey.Key)
