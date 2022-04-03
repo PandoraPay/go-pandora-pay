8815a8a02dff96f52aca0a2b7dbc4c707847af27
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2022-04-03 03:12:05 +0300

    vin and vout builder

--- txs_builder/txs_builder.go
+++ txs_builder/txs_builder.go
@@ -76,7 +76,7 @@ func (builder *TxsBuilder) CreateSimpleTx(txData *TxBuilderCreateSimpleTx, propa
 		txData.Fee = &wizard.WizardTransactionFee{0, 0, 0, true}
 	}
 
-	sendersWalletAddresses, err := builder.getWalletAddresses([]string{txData.Sender})
+	sendersWalletAddresses, err := builder.getWalletAddresses([]string{txData.Vin[0].Sender})
 	if err != nil {
 		return nil, err
 	}
@@ -111,12 +111,31 @@ func (builder *TxsBuilder) CreateSimpleTx(txData *TxBuilderCreateSimpleTx, propa
 	txData.Nonce = builder.getNonce(txData.Nonce, sendersWalletAddresses[0].PublicKey, plainAcc.Nonce)
 	statusCallback("Getting Nonce from Mempool")
 
+	vin := make([]*wizard.WizardTxSimpleTransferVin, len(txData.Vin))
+	for i, v := range txData.Vin {
+		vin[i] = &wizard.WizardTxSimpleTransferVin{
+			sendersWalletAddresses[i].PrivateKey.Key,
+			v.Amount,
+			v.Asset,
+		}
+	}
+
+	vout := make([]*wizard.WizardTxSimpleTransferVout, len(txData.Vout))
+	for i, v := range txData.Vout {
+		vout[i] = &wizard.WizardTxSimpleTransferVout{
+			v.PublicKeyHash,
+			v.Amount,
+			v.Asset,
+		}
+	}
+
 	if tx, err = wizard.CreateSimpleTx(&wizard.WizardTxSimpleTransfer{
 		txData.Extra,
 		txData.Data,
 		txData.Fee,
 		txData.Nonce,
-		sendersWalletAddresses[0].PrivateKey.Key,
+		vin,
+		vout,
 	}, false, statusCallback); err != nil {
 		return nil, err
 	}
--- txs_builder/txs_builder_types.go
+++ txs_builder/txs_builder_types.go
@@ -2,11 +2,24 @@ package txs_builder
 
 import "pandora-pay/txs_builder/wizard"
 
+type TxBuilderCreateSimpleTxVin struct {
+	Sender string `json:"sender" msgpack:"sender"`
+	Amount uint64 `json:"amount" msgpack:"amount"`
+	Asset  []byte `json:"asset" msgpack:"asset"`
+}
+
+type TxBuilderCreateSimpleTxVout struct {
+	PublicKeyHash []byte `json:"publicKeyHash" msgpack:"publicKeyHash"`
+	Amount        uint64 `json:"amount" msgpack:"amount"`
+	Asset         []byte `json:"asset" msgpack:"asset"`
+}
+
 type TxBuilderCreateSimpleTx struct {
-	Sender     string                        `json:"sender" msgpack:"sender"`
-	Nonce      uint64                        `json:"nonce" msgpack:"nonce"`
-	Data       *wizard.WizardTransactionData `json:"data" msgpack:"data"`
-	Fee        *wizard.WizardTransactionFee  `json:"fee" msgpack:"fee"`
-	FeeVersion bool                          `json:"feeVersion" msgpack:"feeVersion"`
-	Extra      wizard.WizardTxSimpleExtra    `json:"extra" msgpack:"sender"`
+	Nonce      uint64                         `json:"nonce" msgpack:"nonce"`
+	Data       *wizard.WizardTransactionData  `json:"data" msgpack:"data"`
+	Fee        *wizard.WizardTransactionFee   `json:"fee" msgpack:"fee"`
+	FeeVersion bool                           `json:"feeVersion" msgpack:"feeVersion"`
+	Extra      wizard.WizardTxSimpleExtra     `json:"extra" msgpack:"sender"`
+	Vin        []*TxBuilderCreateSimpleTxVin  `json:"vin" msgpack:"vin"`
+	Vout       []*TxBuilderCreateSimpleTxVout `json:"vout" msgpack:"vout"`
 }
