5f8f6625f3c7902344f86e14241689b31e663460
yuansong862 <yuansong862@proton.me>
2022-09-03 12:24:21 -0500

    Remove old br and gz files

--- builds/electron_helper/server/routes/route_home.go
+++ builds/electron_helper/server/routes/route_home.go
@@ -2,18 +2,20 @@ package routes
 
 import (
 	"encoding/json"
-	"market/config"
 	"net/http"
+	"pandora-pay/config"
 )
 
 func RouteHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	u := &struct {
-		ProtocolVersion uint64 `json:"protocolVersion"`
-		Version         string `json:"version"`
+		Name    string `json:"name"`
+		Version string `json:"version"`
+		Network uint64 `json:"network"`
 	}{
-		config.PROTOCOL_VERSION,
-		config.VERSION,
+		config.NAME,
+		config.VERSION_STRING,
+		config.NETWORK_SELECTED,
 	}
 	json.NewEncoder(w).Encode(u)
 }
